cf3398ef88f2dd2258181138d6eb79b273b4c357
n5i9c0l <nicoleidgorena@gmail.com>
2026-06-10 13:44:46 -0400

    fix: arreglo de resultado que llega al html

--- appClasificacionResiduos/views.py
+++ appClasificacionResiduos/views.py
@@ -13,9 +13,28 @@ def clasificacion(request):
             ruta_imagen = fs.path(nombre_archivo)
             imagen_url = fs.url(nombre_archivo)
 
-            resultado = maincito(ruta_imagen)
-            print("el resultado es: " + str(resultado))
+            resultado_ia = str(maincito(ruta_imagen)).strip()
+            print("el resultado es:", repr(resultado_ia))
+            print("el resultado es: " + str(resultado_ia))
 
+            mapa = {
+                "1": "Reciclable",
+                "2": "No Reciclable",
+                "3": "Aprovechable",
+                "4": "Infeccioso"
+            }
+
+            if resultado_ia in mapa:
+                resultado = {
+                    "ok": True,
+                    "clasificacion": mapa[resultado_ia],
+                    "datos": resultado_ia
+                }
+            else:
+                resultado = {
+                    "ok": False,
+                    "error": f"Respuesta inválida de la IA: {resultado_ia}"
+                }
             request.session["imagen_url"] = imagen_url
             request.session["resultado"] = resultado
 
